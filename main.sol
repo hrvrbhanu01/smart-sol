55280083567b90b35fc79d27e4abc91c160347a7
hrvrbhanu01 <dixitbhanu16048050@gmail.com>
2024-02-26 17:20:39 +0530

    new contract sol

--- main.sol
+++ main.sol
@@ -1,1 +1,21 @@
-nk
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Greeting {
+    string public name;
+    string public greetingPrefix = "Hello";
+
+    constructor(string memory initialName){
+        name = initialName;
+    }
+
+    function setName(string memory newName) public { //public mean it can be called anywhere in the contract
+        name = newName;
+
+    }
+
+    function getGreeting() public view returns (string memory){ //view means this function is not going to change any data onto the Blockchain it will only read data from the Blockchain.
+        return string(abi.encodePacked(greetingPrefix, name));
+    }
+
+}
